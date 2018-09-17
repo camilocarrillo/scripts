cc6bd08064d6c2f18d7f49e4eee016370aeb3188
Camilo Carrillo-Montoya <ccarrillom@gmail.com>
2018-09-17 23:59:20 +0200

    improvementes scan_merge script

--- scan_merge_double_side.sh
+++ scan_merge_double_side.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-echo "use as scan_merge_double_side.sh file1.pdf file2.pdf"
+echo "use as scan_merge_double_side.sh A-sides.pdf B-sides-reversed.pdf"
 
 export tot1=`pdftk $1 dump_data output | grep NumberOfPages | awk '{print $2}'`
 export tot2=`pdftk $2 dump_data output | grep NumberOfPages | awk '{print $2}'`
@@ -11,7 +11,16 @@ echo $tot2
 file1=$1
 file2=$2 
 
-merged=$1_$2
+if [ $tot1 == $tot2 ]
+then
+    echo "both documents have the same number of pages:$tot1"
+else
+    echo "the documents do not have the same number of pages, $file1:$tot1 $file2:$tot2"
+    exit 
+fi
+
+export name1=`echo $1 | sed -e "s|.pdf||g"`
+export mergedname=`echo "$name1-$2"`
 
 j=$tot1
 list=`for i in \`seq 1 $tot1\`
@@ -22,4 +31,5 @@ list=`for i in \`seq 1 $tot1\`
 	done 
 echo` 
 
-echo pdftk A=$file1  B=$file2 cat $list output $merged
+echo "execute:"
+echo pdftk A=$file1  B=$file2 cat $list output $mergedname
